a67d78b6053a12a1f33ee949f24b762d7b0f7a3c
Tomtoxi44 <tommy.angibaud@gmail.com>
2023-03-28 18:30:46 +0200

    Save.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,30 @@
-const Card = () => {
-    return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbizar" />
+import colorCard from './PokemonCard.module.css'
+
+
+const pokemonCard = () => {
+    return <figure className={colorCard.plante}>
+        <img src={pokemon.imgSrc}
+            alt={pok}
+            className={colorCard.cardImg} />
+        <figcaption>bulbasaur</figcaption>
     </figure>
 }
 
-export default Card;
+
+
+
+
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    }
+]
+
+PokemonCard(pokemonList[0])
+
+export default pokeCard;
